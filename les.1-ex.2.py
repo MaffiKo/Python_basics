70920c11d4cd47aaf8f696ea319323829eb4bcf6
Maria <konyakova.marya@gmail.com>
2020-05-19 18:39:49 +0300

    finished ex.1-3

--- les.1-ex.2.py
+++ les.1-ex.2.py
@@ -1,5 +1,7 @@
 user_answer = int(input("Введите секунды для расчёта: "))
 hours = user_answer // 3600
-minutes = (user_answer % 3600) // 60
+minutes = (user_answer % 3600) // 60 #  (user // 60) - (hours * 60)
 sec = user_answer % 60
 print(f"В заданном количесве секунд: {hours:02}:{minutes:02}:{sec:02}")
+
+# -----------------------------------------------------------
